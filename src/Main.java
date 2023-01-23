b80fa9c7737aa0ac187b5cc24da4554c77d4e7b9
MgK720 <igorhg720@gmail.com>
2023-01-23 02:02:28 +0100

    #3.2 main test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import accommodation.HostelFloor;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HostelFloor siema = new HostelFloor(15, 2010, 200000, "Warsaw", "Olejowa", "3b", 4, 15, true, "fajny budynek", 4, 15, true, true, 30, "Fajny pokoj");
+
+        System.out.println(siema);
     }
 }
