fc3105647780599859cc607545e72393afbdce37
= <igorhg720@gmail.com>
2023-01-24 00:03:50 +0100

    #3.6 create method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 import accommodation.HostelFloor;
 import accommodation.Tent;
+import accommodationCRUD.HostelFloorCRUD;
+
 public class Main {
     public static void main(String[] args) {
 
         HostelFloor test = new HostelFloor(15, 2011, 200000, "Warsaw", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
         Tent test2 = new Tent(15, 2011, 200000, "Warsaw","5", true,true,"fajny campsite",4,100,false,true,"Super namiot");
-        HostelFloor test3 = new HostelFloor(20, 2011, 200000, "Warsaw", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
+        HostelFloor test3 = new HostelFloor(20, 2011, 200000, "Szczytno", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
+        HostelFloor test4 = new HostelFloor(20, 2011, 200000, "usun", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
         System.out.println(test);
         System.out.println(test2);
         System.out.println(test3);
 
         System.out.println(test3.getIds());
+
+        HostelFloorCRUD hostelFloorCRUD = new HostelFloorCRUD();
+
+        hostelFloorCRUD.createHostelFloor(test);
+        hostelFloorCRUD.createHostelFloor(test3);
+        hostelFloorCRUD.createHostelFloor(test4);
     }
 }
--- src/accommodationCRUD/HostelFloorCRUD.java
+++ src/accommodationCRUD/HostelFloorCRUD.java
@@ -1,4 +1,22 @@
 package accommodationCRUD;
 
+import accommodation.HostelFloor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HostelFloorCRUD {
+    private List<HostelFloor> hostelFloors;
+    private Map<Integer, HostelFloor> hostelFloorsID = new HashMap<>();
+    public HostelFloorCRUD() {
+
+        hostelFloors = new ArrayList<HostelFloor>();
+    }
+    public void createHostelFloor(HostelFloor hostelFloor) {
+        hostelFloors.add(hostelFloor);
+        hostelFloorsID.put(hostelFloor.getIdOfAccomodation(), hostelFloor);
+
+    }
 }
