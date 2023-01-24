68341a3b5b5be7f6c8e278cf2ba94bb5d9f9f8d0
= <igorhg720@gmail.com>
2023-01-24 01:02:18 +0100

    #3.7 crud hostel

--- src/Main.java
+++ src/Main.java
@@ -25,10 +25,44 @@ public class Main {
 
         List<HostelFloor> hostelFloors = hostelFloorCRUD.readAllHostelFloors();
         System.out.println(hostelFloors);
-
+        System.out.println("_________________________________________________");
         HostelFloor readtest1 = hostelFloorCRUD.readHostelFloor(1);
         HostelFloor readtest2 = hostelFloorCRUD.readHostelFloor(2);
-        System.out.println(readtest1);
-        System.out.println(readtest2);
+        if(readtest1 != null)System.out.println(readtest1);
+        if(readtest2 != null)System.out.println(readtest2);
+        System.out.println("_________________________________________________");
+
+
+        int updateAtId = 3;
+        HostelFloor hostelFloorToUpdate = hostelFloorCRUD.readHostelFloor(updateAtId);
+        hostelFloorToUpdate.setNumberOfFloors(8);
+        int index = hostelFloors.indexOf(hostelFloorToUpdate);
+        hostelFloorCRUD.updateHostelFloor(index, hostelFloorToUpdate);
+
+        int deleteAtId = 1;
+        //HostelFloor hostelFloorToDelete = hostelFloorCRUD.readHostelFloor(deleteAtId);
+        hostelFloorCRUD.deleteHostelFloor(deleteAtId);
+        System.out.println(hostelFloors);
+        //System.out.println(hostelFloorCRUD.getHostelFloorsID());
+
+        HostelFloor test5 = new HostelFloor(20, 2011, 200000, "usun", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
+        hostelFloorCRUD.createHostelFloor(test5);
+        System.out.println(hostelFloors);
+        //System.out.println(hostelFloorCRUD.getHostelFloorsID());
+
+        deleteAtId = 2;
+        hostelFloorCRUD.deleteHostelFloor(deleteAtId);
+        //System.out.println(hostelFloors);
+
+        updateAtId = 1;
+        hostelFloorToUpdate = hostelFloorCRUD.readHostelFloor(updateAtId);
+        if(hostelFloorToUpdate != null){
+            hostelFloorToUpdate.setNumberOfFloors(6);
+            index = hostelFloors.indexOf(hostelFloorToUpdate);
+            hostelFloorCRUD.updateHostelFloor(index, hostelFloorToUpdate);
+        }
+
+        HostelFloor readtest3 = hostelFloorCRUD.readHostelFloor(1);
+        if(readtest3 != null){System.out.println(readtest3);}
     }
 }
--- src/accommodationCRUD/HostelFloorCRUD.java
+++ src/accommodationCRUD/HostelFloorCRUD.java
@@ -9,6 +9,9 @@ import java.util.Map;
 
 public class HostelFloorCRUD {
     private List<HostelFloor> hostelFloors;
+
+    public Map<Integer, HostelFloor> getHostelFloorsID() {return hostelFloorsID;}
+
     private Map<Integer, HostelFloor> hostelFloorsID = new HashMap<>();
     public HostelFloorCRUD() {
 
@@ -23,6 +26,26 @@ public class HostelFloorCRUD {
     }
 
     public HostelFloor readHostelFloor(int id) {
+        if (!hostelFloorsID.containsKey(id)) {
+            System.out.println("Hostel floor with id " + id + " does not exist.");
+            return null;
+        }
         return hostelFloorsID.get(id);
+    }
+    public void updateHostelFloor(int index, HostelFloor hostelFloor) {
+        if (!hostelFloorsID.containsKey(index)) {
+            System.out.println("Hostel floor with id " + index + " does not exist.");
+            return;
+        }
+        hostelFloors.set(index, hostelFloor);
+    }
+    public void deleteHostelFloor(int id) {
+        if (!hostelFloorsID.containsKey(id)) {
+            System.out.println("Hostel floor with id " + id + " does not exist.");
+            return;
+        }
+        int index = hostelFloors.indexOf(hostelFloorsID.get(id));
+        hostelFloorsID.remove(id);
+        hostelFloors.remove(index);
     }
 }
