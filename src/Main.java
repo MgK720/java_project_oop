ca3647d9d330acffce7d120738b1f17d2b249d56
= <igorhg720@gmail.com>
2023-01-29 19:40:15 +0100

    #4.2 main update

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        HostelFloor test = new HostelFloor(15, 2015, 200000, "Warsaw", "Olejowa", "3b", 4, 35, false, "fajny budynek", 1, 15, true, false, 30, "Fajny pokoj");
+        HostelFloor test = new HostelFloor(15, 2015, 200000, "Warsaw", "Olejowa", "3b", 4, 35, true, "fajny budynek", 1, 15, true, false, 30, "Fajny pokoj");
         Tent test2 = new Tent(15, 2011, 200000, "Warsaw","5", true,true,"fajny campsite",4,100,false,true,"Super namiot");
-        HostelFloor test3 = new HostelFloor(20, 2011, 200000, "Szczytno", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
+        HostelFloor test3 = new HostelFloor(20, 1980, 200000, "Szczytno", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
         HostelFloor test4 = new HostelFloor(20, 2011, 200000, "usun", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
-        System.out.println(test);
-        System.out.println("a");
-        System.out.println(test2);
-        System.out.println(test3);
-
-        System.out.println(test3.getIds());
 
         HostelFloorCRUD hostelFloorCRUD = new HostelFloorCRUD();
 
@@ -24,44 +18,56 @@ public class Main {
         hostelFloorCRUD.createHostelFloor(test3);
         hostelFloorCRUD.createHostelFloor(test4);
 
+        System.out.println("_________________________________________________________________________");
         List<HostelFloor> hostelFloors = hostelFloorCRUD.readAllHostelFloors();
-        System.out.println(hostelFloors);
+        System.out.println("PRINT ALL HOSTEL FLOORS:\n" + hostelFloors);
 
-        System.out.println("_________________________________________________");
-        HostelFloor readtest1 = hostelFloorCRUD.readHostelFloor(1);
-        HostelFloor readtest2 = hostelFloorCRUD.readHostelFloor(2);
-        if(readtest1 != null)System.out.println(readtest1);
-        if(readtest2 != null)System.out.println(readtest2);
-        System.out.println("_________________________________________________");
-
+        System.out.println("_________________________________________________________________________");
+        int readAtID = 1;
+        if(hostelFloorCRUD.hostelFloorExists(readAtID)){
+            HostelFloor readtest = hostelFloorCRUD.readHostelFloor(readAtID);
+            System.out.println("READED HOSTEL FLOOR AT INDEX ["+readAtID+"]:\n"+readtest);
+        }else {hostelFloorCRUD.printHostelFloorNotFound(readAtID);}
+        readAtID = 2;
+        if(hostelFloorCRUD.hostelFloorExists(readAtID)){
+            HostelFloor readtest = hostelFloorCRUD.readHostelFloor(readAtID);
+            System.out.println("READED HOSTEL FLOOR AT INDEX ["+readAtID+"]:\n"+readtest);
+        }else {hostelFloorCRUD.printHostelFloorNotFound(readAtID);}
+        System.out.println("_________________________________________________________________________");
 
         int updateAtId = 3;
         HostelFloor hostelFloorToUpdate = null;
         int index = 0;
         if(hostelFloorCRUD.hostelFloorExists(updateAtId)){
             hostelFloorToUpdate = hostelFloorCRUD.readHostelFloor(updateAtId);
-            hostelFloorToUpdate.setNumberOfFloors(8);
+            hostelFloorToUpdate.setYearOfConstruction(2022);
             index = hostelFloors.indexOf(hostelFloorToUpdate);
             hostelFloorCRUD.updateHostelFloor(index, hostelFloorToUpdate);
+            System.out.println("UPDATED HOSTEL FLOOR AT INDEX ["+updateAtId+"]\n"+hostelFloorToUpdate);
         }else {hostelFloorCRUD.printHostelFloorNotFound(updateAtId);}
+        System.out.println("_________________________________________________________________________");
 
         int deleteAtId = 1;
         if(hostelFloorCRUD.hostelFloorExists(deleteAtId)){
             //HostelFloor hostelFloorToDelete = hostelFloorCRUD.readHostelFloor(deleteAtId);
             hostelFloorCRUD.deleteHostelFloor(deleteAtId);
+            System.out.println("FLOOR AT INDEX ["+deleteAtId+"] IS DELETED");
             System.out.println(hostelFloors);
             //System.out.println(hostelFloorCRUD.getHostelFloorsID());
         }else {hostelFloorCRUD.printHostelFloorNotFound(deleteAtId);}
+        System.out.println("_________________________________________________________________________");
 
         HostelFloor test5 = new HostelFloor(20, 2011, 200000, "usun", "Olejowa", "3b", 4, 30, false, "fajny budynek", 1, 15, false, false, 500, "Fajny pokoj");
         hostelFloorCRUD.createHostelFloor(test5);
-        System.out.println(hostelFloors);
+        System.out.println("ADDED NEW HOSTEL FLOOR, PRINT ALL HOSTEL FLOORS:\n" + hostelFloors);
         //System.out.println(hostelFloorCRUD.getHostelFloorsID());
+        System.out.println("_________________________________________________________________________");
 
         deleteAtId = 2;
             if(hostelFloorCRUD.hostelFloorExists(deleteAtId)){
-            hostelFloorCRUD.deleteHostelFloor(deleteAtId);
-            //System.out.println(hostelFloors);
+                hostelFloorCRUD.deleteHostelFloor(deleteAtId);
+                System.out.println("FLOOR AT INDEX ["+deleteAtId+"] IS DELETED");
+                System.out.println(hostelFloors);
         }else {hostelFloorCRUD.printHostelFloorNotFound(deleteAtId);}
 
         updateAtId = 1;
@@ -70,13 +76,14 @@ public class Main {
             hostelFloorToUpdate.setNumberOfFloors(6);
             index = hostelFloors.indexOf(hostelFloorToUpdate);
             hostelFloorCRUD.updateHostelFloor(index, hostelFloorToUpdate);
+            System.out.println("UPDATED HOSTEL FLOOR AT INDEX ["+updateAtId+"]\n"+hostelFloorToUpdate);
         }else {hostelFloorCRUD.printHostelFloorNotFound(updateAtId);}
 
 
-        int readAtID = 1;
+        readAtID = 1;
             if(hostelFloorCRUD.hostelFloorExists(readAtID)){
-            HostelFloor readtest3 = hostelFloorCRUD.readHostelFloor(readAtID);
-            System.out.println(readtest3);
+            HostelFloor readtest = hostelFloorCRUD.readHostelFloor(readAtID);
+                System.out.println("READED HOSTEL FLOOR AT INDEX ["+readAtID+"]:\n"+readtest);
         }else {hostelFloorCRUD.printHostelFloorNotFound(readAtID);}
     }
 }
